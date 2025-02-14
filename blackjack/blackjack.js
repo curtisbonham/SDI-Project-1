c4c4889ddb2357486d7903da7698dc62b97fb3fa
curtisbonham <curtis.bonham1@gmail.com>
2025-02-14 16:14:20 -0700

    added more functionality

--- blackjack/blackjack.js
+++ blackjack/blackjack.js
@@ -1,21 +1,18 @@
 let deckID = "";
 let dealerCards = [];
 let playerCards = [];
-let dealerSum = "";
-let playerSum = 0;
+let dealerScore = "";
+let playerScore = 0;
 let roundWon = false;
 let roundLost = false;
 let roundTied = false;
-hitArea.style.display = "none";
-stayArea.style.display = "none";
-newGameArea.style.display = "none";
 
 let dealerAceCount = 0;
 let playerAceCount = 0;
 
 //score displays areas
-let dealerScoreArea = document.getElementById("dealerSum");
-let playerScoreArea = document.getElementById("playerSum");
+let dealerScoreArea = document.getElementById("dealerScore");
+let playerScoreArea = document.getElementById("playerScore");
 
 //card display areas
 let dealerCardsArea = document.getElementById("dealerCards");
@@ -27,10 +24,29 @@ let startPlayArea = document.getElementById("startPlayBtn");
 let newGameArea = document.getElementById("newGameBtn");
 let hitArea = document.getElementById("hitBtn");
 let stayArea = document.getElementById("stayBtn");
+hitArea.style.display = "none";
+stayArea.style.display = "none";
+newGameArea.style.display = "none";
 
 //on-click events
-startPlayArea.onclick = getNewDeck();
-newGameArea.onclick = newGame();
+startPlayArea.addEventListener("click", () => getNewDeck());
+newGameArea.addEventListener("click", () => newGame());
+hitArea.addEventListener("click", () => hit("player"));
+stayArea.addEventListener("click", () => setTimeout(() => dealersTurn(), 600));
+
+const resetPlayingArea = () => {
+  dealerCards = [];
+  playerCards = [];
+  roundWon = false;
+  roundLost = false;
+  roundTied = false;
+  dealerScore = "";
+  playerScore = 0;
+  dealerScoreArea.textContent = dealerScore;
+  resultsArea.textContent = "";
+  dealerCardsArea.textContent = "";
+  playerCardsArea.textContent = "";
+}
 
 const newGame = () => {
   resetPlayingArea();
@@ -42,6 +58,8 @@ const newGame = () => {
 
     dealerScore = "?";
     dealerScoreArea.textContent = dealerScore;
+    hitArea.style.display = "block";
+    stayArea.style.display = "block";
 
     dealerCards.forEach((card, i) => {
       let cardImage = document.createElement("img");
@@ -58,10 +76,11 @@ const newGame = () => {
       cardImage.src = card.image;
       playerCardsArea.appendChild(cardImage);
     })
-
+    console.log(playerCards);
     playerScore = calculateScore(playerCards);
     if(playerScore === 21) {
       roundWon = true;
+      resultsArea.textContent = "Blackjack! You win the hand!"
     }
     playerScoreArea.textContent = playerScore;
 })
@@ -81,54 +100,77 @@ const getNewDeck = () => {
   .catch(console.error)
 }
 
-const hit = () => {
+const hit = (target) => {
   fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`)
   .then(response => response.json())
   .then(cardData => {
+
+  if(target === "player"){
   playerCards.push(cardData.cards[0])
+  let cardImage = document.createElement("img");
+  cardImage.src = cardData.cards[0].image;
+  playerCardsArea.appendChild(cardImage);
+  playerScore = calculateScore(playerCards);
+  playerScoreArea.textContent = playerScore;
+  if (playerScore > 21) {
+    roundLost = true;
+    resultsArea.textContent = "You busted!! You lose the hand!!"
+  }
+}
+  if(target === "dealer"){
+    let cardImage = document.createElement("img");
+    dealerCards.push(cardData.cards[0]);
+    cardImage.src = cardData.cards[0].image;
+    dealerCardsArea.appendChild(cardImage);
+    dealersTurn();
+  }
 })
+.catch(console.log)
 }
 
-const calculateScore = () => {
-  let cardCode = [];
-      for(let key in cardData){
-        for(let subKey in cardData[key]){
-          if (cardData[key][subKey].code === undefined){
-            continue;
-          }
-          cardCode.push(cardData[key][subKey].code[0])}
-        }
-      cardCode.forEach(value => {
-        if (isNaN(value)){
-          if (value === "A"){
-            console.log(11)
-            return 11;
-          }
-          console.log(10)
-          return 10;
-        }
-        if(!isNaN(value)){
-          if(value === "0"){
-            console.log(10);
-          return 10;
-          }
-          console.log(parseInt(value))
-          return parseInt(value);
-        }
-      })
-    }
+const dealersTurn = () => {
+  if (roundLost || roundWon || roundTied) {return}
+  dealerScore = calculateScore(dealerCards);
+  dealerScoreArea.textContent = dealerScore;
+  dealerScoreArea.firstChild.src = dealerCards[0].image;
+  if (dealerScore < 17) {
+    setTimeout(()=>hitMe('dealer'), 900)
+  }
+  else if (dealerScore > 21) {
+    roundWon = true;
+    resultsArea.textContent = "House broke. You Won the hand!";
+  }
+  else if (dealerScore > playerScore) {
+    roundLost = true;
+    resultsArea.textContent = "You Lost the hand...";
+  }
+  else if (dealerScore === playerScore) {
+    roundTied = true;
+    resultsArea.textContent = "Tie round...";
+  }
+  else {
+    roundWon = true;
+    resultsArea.textContent = "You Win the hand!";
+  }
+}
 
-    const resetPlayingArea = () => {
-      dealerCards = [];
-      playerCards = [];
-      roundWon = false;
-      roundLost = false;
-      roundTied = false;
-      dealerSum = "";
-      playerSum = 0;
-      dealerScoreArea.textContent = dealerScore;
-      resultsArea.textContent = "";
+const calculateScore = (cards) => {
+  let hasAce = false;
+  score = cards.reduce((acc, card) => {
+    if (card.value === "ACE") {
+      hasAce = true;
+      return acc + 1
     }
+    if (isNaN(card.value)) { return acc + 10 }
+    return acc + Number(card.value);
+  }, 0)
+  if (hasAce) {
+    score = (score + 10) > 21 ? score : score + 10;
+  }
+  return score
+}
+
+
 
 
 
