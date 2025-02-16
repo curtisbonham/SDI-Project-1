d4fea99a0a9e772fdcfef6681669023d7e76e35f
curtisbonham <curtis.bonham1@gmail.com>
2025-02-16 15:07:18 -0700

    added more coins to the ticker

--- crypto.js
+++ crypto.js
@@ -4,13 +4,18 @@ const cryptoTicker = () => {
   fetch('https://api.coinlore.net/api/tickers/')
     .then(response => response.json())
     .then(currencies => {
-      let top5 = currencies.data.splice(0, 5)
+      let top10 = currencies.data.splice(0, 10)
       tickerTape.textContent =
-        (`${top5[0].name} (${top5[0].symbol}) $${top5[0].price_usd}usd (${top5[0].percent_change_24h}%) * * *
-        ${top5[1].name} (${top5[1].symbol}) $${top5[1].price_usd}usd (${top5[1].percent_change_24h}%) * * *
-        ${top5[2].name} (${top5[2].symbol}) $${top5[2].price_usd}usd (${top5[2].percent_change_24h}%) * * *
-        ${top5[3].name} (${top5[3].symbol}) $${top5[3].price_usd}usd (${top5[3].percent_change_24h}%) * * *
-        ${top5[4].name} (${top5[4].symbol}) $${top5[4].price_usd}usd (${top5[4].percent_change_24h}%)`)
+        (`${top10[0].name} (${top10[0].symbol}) $${top10[0].price_usd}usd (${top10[0].percent_change_24h}%) * * *
+        ${top10[1].name} (${top10[1].symbol}) $${top10[1].price_usd}usd (${top10[1].percent_change_24h}%) * * *
+        ${top10[2].name} (${top10[2].symbol}) $${top10[2].price_usd}usd (${top10[2].percent_change_24h}%) * * *
+        ${top10[3].name} (${top10[3].symbol}) $${top10[3].price_usd}usd (${top10[3].percent_change_24h}%) * * *
+        ${top10[4].name} (${top10[4].symbol}) $${top10[4].price_usd}usd (${top10[4].percent_change_24h}%) * * *
+        ${top10[5].name} (${top10[5].symbol}) $${top10[5].price_usd}usd (${top10[5].percent_change_24h}%) * * *
+        ${top10[6].name} (${top10[6].symbol}) $${top10[6].price_usd}usd (${top10[6].percent_change_24h}%) * * *
+        ${top10[7].name} (${top10[7].symbol}) $${top10[7].price_usd}usd (${top10[7].percent_change_24h}%) * * *
+        ${top10[8].name} (${top10[8].symbol}) $${top10[8].price_usd}usd (${top10[8].percent_change_24h}%) * * *
+        ${top10[9].name} (${top10[9].symbol}) $${top10[9].price_usd}usd (${top10[9].percent_change_24h}%)`)
     })
   }
 
